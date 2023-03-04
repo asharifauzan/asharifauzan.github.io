ef413642e0fdb9ac064f9b87c036568796807508
Fauzan Hibatullah Ashari <fauzansaja10@gmail.com>
2023-03-04 14:42:27 -0500

    test: high school education

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react';
 
-test('renders learn react link', () => {
+test('render high school education', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/smkn 3 depok/i);
   expect(linkElement).toBeInTheDocument();
 });
